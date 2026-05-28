60caf9896cb7cfd07d08333ccadb7276e340a75b
coutinhojessica <jessicamaiara.coutinho@gmail.com>
2026-05-27 21:39:53 -0300

    feat: concluída a página hero.

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,8 +1,14 @@
 export function iniciarTheme() {
-    const btnTheme = document.querySelector('.btnTheme');
-    let headerTheme = document.getElementById('header');
-    btnTheme.addEventListener('click', () =>{
-        headerTheme.classList.toggle('dark');
-        headerTheme.classList.toggle('light');
-    })
+  const btnTheme = document.querySelector(".btnTheme");
+  let headerTheme = document.getElementById("header");
+  const heroTheme = document.getElementById("hero");
+  btnTheme.addEventListener("click", () => {
+    // page header
+    headerTheme.classList.toggle("dark");
+    headerTheme.classList.toggle("light");
+
+    // page hero
+    heroTheme.classList.toggle("dark");
+    heroTheme.classList.toggle("light");
+  });
 }
